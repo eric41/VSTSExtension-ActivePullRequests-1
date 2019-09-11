ab728f2fa0d30b1a4d97c12ecdca6ea079d89d22
eric41 <eric@obi4wan.nl>
2019-09-11 10:47:28 +0200

    Add JelleVisser changes (since you cannot fork a fork, apparently) and remove the build call since we're not using it

--- src/app.models.ts
+++ src/app.models.ts
@@ -1,22 +1,38 @@
 /// <reference types="vss-web-extension-sdk" />
 import { IdentityRefWithVote } from "TFS/VersionControl/Contracts";
 import { IdentityRef } from "VSS/WebApi/Contracts";
+import { Build } from "TFS/Build/Contracts";
 
 export interface PullRequest {
-    createdBy: IdentityRef;
-    id: number;
-    baseUri: string;
-    projectName: string;
-    title: string;
-    repo: string;
-    vote: number;
-    reviewers: IdentityRefWithVote[];
-    baseBranch: string;
-    targetBranch: string;
+  createdBy: IdentityRef;
+  id: number;
+  baseUri: string;
+  projectName: string;
+  title: string;
+  repo: string;
+  vote: number;
+  reviewers: IdentityRefWithVote[];
+  baseBranch: string;
+  targetBranch: string;
+  isDraft: boolean;
+  creationDate: Date;
+}
+
+export interface PullRequestWithBuild {
+  pr: PullRequest;
+  build: Build;
 }
 
 export interface Vote {
-    icon: string;
-    message: string;
-    color?: string;
+  icon: string;
+  message: string;
+  showIconInList: boolean;
+  order: number;
+  color?: string;
+}
+
+export interface BuildDisplay {
+  icon?: string;
+  message: string;
+  color?: string;
 }
--- src/app.ts
+++ src/app.ts
@@ -2,48 +2,69 @@ import { VssPullRequests } from "./vss-pull-requests.service";
 declare var sorttable: any;
 
 const prClient = new VssPullRequests();
-prClient.getPullRequests().then(prs => {
+prClient.getPullRequests().then(PRs => {
     // Build the table
     const tableHeader = document.getElementById("pr-header");
-    tableHeader.innerText += " (" + prs.length + " pull request" + (prs.length === 1 ? "" : "s") + ")";
+    tableHeader.innerText += " (" + PRs.length + " pull request" + (PRs.length === 1 ? "" : "s") + ")";
     const tableBody = document.getElementById("pr-body");
-    console.log("*** pull request data ***", prs);
-    prs.forEach(pr => {
-        const repoUrl = pr.baseUri + encodeURIComponent(pr.projectName) + "/_git/" + encodeURIComponent(pr.repo);
+    console.log("*** pull request data ***", PRs);
+    PRs.forEach(PR => {
+        const repoUrl = PR.baseUri + encodeURIComponent(PR.projectName) + "/_git/" + encodeURIComponent(PR.repo);
         const tableRow = document.createElement("tr");
         tableRow.classList.add("table-row");
         // User Avatar cell
         const tableCellUserAvatar = document.createElement("td");
-        tableCellUserAvatar.innerHTML = "<img class='user-avatar' src='" + pr.createdBy.imageUrl + "' alt='" + pr.createdBy.uniqueName + "\'s avatar' />";
+        tableCellUserAvatar.innerHTML = "<img class='user-avatar' src='" + PR.createdBy.imageUrl + "' alt='" + PR.createdBy.uniqueName + "\'s avatar' />";
         tableRow.appendChild(tableCellUserAvatar);
         // Created By cell
         const tableCellUser = document.createElement("td");
-        tableCellUser.innerText = pr.createdBy.displayName;
+        tableCellUser.innerText = PR.createdBy.displayName;
         tableRow.appendChild(tableCellUser);
+        // Creation Date cell
+        const tableCellCreationDate = document.createElement("td");
+        const age = Math.floor((Date.now() - PR.creationDate.getTime()) / 36e5 * 10) / 10;
+        tableCellCreationDate.setAttribute("sorttable_customkey", "" + age);
+        if (age >= 24) {
+            tableCellCreationDate.classList.add("u-danger");
+        }
+        tableCellCreationDate.innerText = `${age} ${ age > 1 ? "hours" : "hour"}`;
+        tableRow.appendChild(tableCellCreationDate);
+
         // Pull Request ID cell
         const tableCellId = document.createElement("td");
-        tableCellId.setAttribute("sorttable_customkey", "" + pr.id);
-        tableCellId.innerHTML = "<a href='" + repoUrl + "/pullRequest/" + pr.id + "' target='_top'>#" + pr.id + "</a>";
+        tableCellId.setAttribute("sorttable_customkey", "" + PR.id);
+        tableCellId.innerHTML = "<a href='" + repoUrl + "/pullRequest/" + PR.id + "' target='_top'>#" + PR.id + "</a>";
         tableRow.appendChild(tableCellId);
         // Title cell
         const tableCellTitle = document.createElement("td");
-        tableCellTitle.innerText = pr.title;
+        if (PR.isDraft) {
+            tableCellTitle.classList.add("u-danger");
+        }
+        tableCellTitle.innerText = `${PR.isDraft ? "[DRAFT]" : ""} ${PR.title}`;
         tableRow.appendChild(tableCellTitle);
         // Repository cell
         const tableCellRepo = document.createElement("td");
-        tableCellRepo.innerText = pr.repo;
+        tableCellRepo.innerText = PR.repo;
         tableRow.appendChild(tableCellRepo);
-        // Base cell
-        const tableCellBaseBranch = document.createElement("td");
-        tableCellBaseBranch.innerHTML = "<a href='" + repoUrl + "?version=GB" + encodeURIComponent(pr.baseBranch) + "' target='_top'>" + pr.baseBranch + "</a>";
-        tableRow.appendChild(tableCellBaseBranch);
-        // Target cell
-        const tableCellTargetBranch = document.createElement("td");
-        tableCellTargetBranch.innerHTML = "<a href='" + repoUrl + "?version=GB" + encodeURIComponent(pr.targetBranch) + "' target='_top'>" + pr.targetBranch + "</a>";
-        tableRow.appendChild(tableCellTargetBranch);
+        // Base cell -- Don't need it
+        // const tableCellBaseBranch = document.createElement("td");
+        // tableCellBaseBranch.innerHTML = `<a href="${repoUrl}?version=GB${encodeURIComponent(PR.baseBranch)}" target="_top"><span class="bowtie-icon bowtie-tfvc-branch"></span>${PR.baseBranch}</a>`;
+        // tableRow.appendChild(tableCellBaseBranch);
+        // Target cell -- Don't need it
+        // const tableCellTargetBranch = document.createElement("td");
+        // tableCellTargetBranch.innerHTML = `<a href="${repoUrl}?version=GB${encodeURIComponent(PR.targetBranch)}" target="_top"><span class="bowtie-icon bowtie-tfvc-branch"></span>${PR.targetBranch}</a>`;
+        // tableRow.appendChild(tableCellTargetBranch);
+        // Build Status cell -- Don't need it
+        // const tableCellBuildStatus = document.createElement("td");
+        // const buildDisplay = prClient.buildStatusToBuildDisplay(prBuild.build);
+        // tableCellBuildStatus.setAttribute("sorttable_customkey", (prBuild.build != null ? prBuild.build.status : 0).toString());
+        // tableCellBuildStatus.innerHTML = (buildDisplay.icon != null ? `<span class="icon bowtie-icon bowtie-${buildDisplay.icon}"></span> ` : "") + buildDisplay.message;
+        // tableCellBuildStatus.style.color = buildDisplay.color != null ? buildDisplay.color : "#808080";
+        // tableRow.appendChild(tableCellBuildStatus);
         // My Vote cell
         const tableCellVote = document.createElement("td");
-        const vote = prClient.voteNumberToVote(pr.vote);
+        tableCellVote.setAttribute("sorttable_customkey", "" + PR.vote);
+        const vote = prClient.voteNumberToVote(PR.vote);
         tableCellVote.innerHTML = vote.icon + " " + vote.message;
         if (vote.color !== undefined) {
             tableCellVote.style.color = vote.color;
@@ -52,13 +73,14 @@ prClient.getPullRequests().then(prs => {
         // Reviewers cell
         const tableCellReviewers = document.createElement("td");
         tableCellReviewers.classList.add("reviewers-cell");
-        tableCellReviewers.setAttribute("sorttable_customkey", "" + pr.reviewers.length);
-        pr.reviewers.forEach(reviewer => {
-            const reviewerVote = prClient.voteNumberToVote(reviewer.vote);
+        tableCellReviewers.setAttribute("sorttable_customkey", "" + PR.reviewers.length);
+        PR.reviewers.map(x => {
+            return { vote: prClient.voteNumberToVote(x.vote), reviewer: x };
+        }).sort(x => x.vote.order).forEach(reviewerVote => {
             const reviewerElement = document.createElement("span");
             reviewerElement.classList.add("reviewer-icon");
-            reviewerElement.innerHTML += "<img class='user-avatar' src='" + reviewer.imageUrl + "' alt=\"" + reviewer.uniqueName + "'s avatar\" />";
-            reviewerElement.innerHTML += "<div class='vote-icon'>" + reviewerVote.icon + "</div>";
+            reviewerElement.innerHTML += `<img class="user-avatar" src="${reviewerVote.reviewer.imageUrl}" alt="${reviewerVote.reviewer.uniqueName}'s avatar" />`;
+            reviewerElement.innerHTML += "<div class='vote-icon'>" + reviewerVote.vote.icon + "</div>";
             tableCellReviewers.appendChild(reviewerElement);
         });
         tableRow.appendChild(tableCellReviewers);
